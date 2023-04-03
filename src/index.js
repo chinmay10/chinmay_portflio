fb52f1d94cbea43754d6c04f6ce38118da6bca74
chinmay10 <chinmay997@gmail.com>
2023-04-02 23:51:21 -0500

    changed websites

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import "normalize.css";
-import "./scss/main.ltr.scss";
-import "./scss/themes.scss";
-import "./scss/purple.scss";
-import "./scss/custom.scss";
+import { render } from 'react-dom'
+import App from './App'
+import { ThemeProvider } from './contexts/theme'
+import './index.css'
 
-import imagesLoaded from "imagesloaded";
-
-import SiteState from './js/SiteState';
-import Site from './js/Site';
-
-imagesLoaded('.preloadimage', function() {    
-    const siteState = new SiteState();
-    new Site(siteState);
-});
+render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+)
